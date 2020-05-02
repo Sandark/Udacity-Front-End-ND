6e75e2c8dceef75d4eb56ce6d7ba4edc82a4d558
Anton Verchenko <anton.verch@gmail.com>
2020-05-02 11:30:13 +0300

    nodejs weather jounrla

---
+++ server.js
@@ -0,0 +1,44 @@
+// Using express to run the server
+const express = require("express");
+
+// Loading app using express
+const app = express();
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const cors = require("cors");
+
+app.use(cors());
+
+// Load client from specified package
+app.use(express.static("client"));
+
+const port = 8080;
+
+const server = app.listen(port, () => {
+    console.log(`Hello! Server is running on port ${port}`)
+});
+
+let projectData = {};
+
+app.post("/add", (req, res) => {
+    projectData = {
+        date: getCurrentDate(),
+        temp: req.body.temp,
+        feelings: req.body.feelings
+    }
+
+    res.json(projectData);
+});
+
+const getCurrentDate = () => {
+    let d = new Date();
+    return d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+}
+
+app.get("/record", (req, res) => {
+    res.json(projectData);
+})
