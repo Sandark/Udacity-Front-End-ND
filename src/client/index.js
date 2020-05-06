223a485519b4d88dc5e7c5a6d399d856d1de474c
Anton <anton.verch@gmail.com>
2020-05-06 22:16:18 +0300

    split scss files into smaller chunks

--- src/client/index.js
+++ src/client/index.js
@@ -3,7 +3,11 @@ import "regenerator-runtime/runtime";
 import "./js/main";
 import {postRequest} from "./js/httpRequest";
 
+import "./styles/global.scss";
 import "./styles/base.scss";
+import "./styles/buttons.scss";
+import "./styles/results.scss";
+import "./styles/loader.scss";
 
 export {
     postRequest
